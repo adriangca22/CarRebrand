94dccff5f36fee33698e5e495e8fcc8e1e03e3ac
adriangca22 <131614373+adriangca22@users.noreply.github.com>
2025-02-28 07:50:39 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // Asegúrate de que esta ruta sea correcta para tu despliegue
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',  // Configuración para proxy si es necesario
+      '/api': 'http://localhost:5000', // Configuración para proxy si es necesario
     }
   },
   build: {
-    outDir: 'dist',  // Carpeta donde se generará el build
+    outDir: 'dist', // Carpeta donde se generará el build
   },
 });
+
